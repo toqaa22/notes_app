1e5c06d6018e986d419b0b95c7a61e0dd0ef6352
toqaa22 <tokatota2014@gmail.com>
2023-04-06 00:56:40 +0200

    create notemodel

--- lib/view/widgets/editnoteview_body.dart
+++ lib/view/widgets/editnoteview_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/view/widgets/custom_appbar.dart';
+import 'package:note/view/widgets/custom_textfield.dart';
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
 
@@ -8,7 +9,14 @@ class EditNoteViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(26.0),
       child: Column(children: [
-        CustomAppbar(icon: Icons.edit, title: 'Edit Note'),
+        CustomAppbar(
+            icon: Icons.edit, title: 'Edit Note'),
+        SizedBox(height: 24,),
+        CustomTextField(hintText: 'Title'),
+        SizedBox(
+          height: 30,
+        ),
+        CustomTextField(hintText: 'Content',maxLines: 5),
       ],),
     );
   }
